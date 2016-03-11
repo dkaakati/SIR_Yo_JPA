c9c3f368d7eb9d2fc5b2e532f23398a2ee13c76d
Dania Kaakati <dania.kaakati@gmail.com>
2016-03-11 12:38:06 +0100

    Finishing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,32 +9,32 @@
  * Main module of the application.
  */
 angular
-  .module('sirApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/person', {
-        templateUrl: 'views/person.html',
-        controller: 'PersonCtrl',
-        controllerAs: 'person'
-      })
-        .when('/addPerson', {
-          templateUrl: 'views/addPerson.html',
-          controller: 'PersonPost',
-          controllerAs: 'person'
-        })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('sirApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+      $routeProvider
+          .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+          })
+          .when('/person', {
+            templateUrl: 'views/person.html',
+            controller: 'PersonCtrl',
+            controllerAs: 'person'
+          })
+          .when('/addPerson', {
+            templateUrl: 'views/addPerson.html',
+            controller: 'PersonPost',
+            controllerAs: 'person'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+    });
--- app/scripts/controllers/person.js
+++ app/scripts/controllers/person.js
@@ -9,49 +9,47 @@
  * Controller of the sirApp
  */
 angular.module('sirApp')
-  .controller('PersonCtrl', user).controller('PersonPost', postUser);
+    .controller('PersonCtrl', user).controller('PersonPost', postUser);
 
 function user($scope, $http,$route){
-  $http({
-    method: 'GET',
-    url: "/rest/hello/persons/"
-  }).then(function successCallback(response) {
-    var data ;
-    if(response.data.person.name){
-      data = [];
-      data.push(response.data.person);
-      console.log(data)
-     }else{
-      data =  response.data.person;
-     }
-     $scope.personlists = data;
-
-  });
-
-  $scope.deletePers = function(id){
     $http({
-      method: 'DELETE',
-      url: "/rest/hello/persons/"+id,
-
+        method: 'GET',
+        url: "/rest/hello/persons/"
     }).then(function successCallback(response) {
-      console.log("hhhh");
-      $route.reload();
+        var data ;
+        if(response.data.person.name){
+            data = [];
+            data.push(response.data.person);
+        }else{
+            data =  response.data.person;
+        }
+        $scope.personlists = data;
+
     });
-  }
+
+    $scope.deletePers = function(id){
+        $http({
+            method: 'DELETE',
+            url: "/rest/hello/persons/"+id,
+
+        }).then(function successCallback(response) {
+            $route.reload();
+        });
+    }
 }
 
 function postUser($scope, $http, $location){
-  $scope.user = {};
- $scope.submitForm = function(){
-   $http({
-     method: 'POST',
-     url: "/rest/hello/persons/",
-     data : $scope.user,
+    $scope.user = {};
+    $scope.submitForm = function(){
+        $http({
+            method: 'POST',
+            url: "/rest/hello/persons/",
+            data : $scope.user,
 
-   }).then(function successCallback(response) {
-      $location.path("/person")
-   });
- }
+        }).then(function successCallback(response) {
+            $location.path("/person")
+        });
+    }
 
 }
 
